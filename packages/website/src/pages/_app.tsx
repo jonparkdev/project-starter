29e93d71938de1c25eba35023d559a25ed9c2274
Jon Park <jon@jonathanpark.ca>
2022-04-13 22:05:09 -0400

    test lint-staged

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+const test = (a: string, b: number): string => a;
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const test1 = test(2, 3);
   return <Component {...pageProps} />;
 }
 
