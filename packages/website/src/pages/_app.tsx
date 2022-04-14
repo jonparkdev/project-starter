d470db2b9c3906b6f9d23f07fdb20ccf8df66508
Jon Park <jon@jonathanpark.ca>
2022-04-13 22:16:40 -0400

    undo pre commit changes

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-const test = (a: string, b: number): string => console.log(a);
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const test1 = test(2, "test");
   return <Component {...pageProps} />;
 }
 
