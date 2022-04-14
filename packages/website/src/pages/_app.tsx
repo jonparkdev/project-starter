abbfa2feaf091dfed8039b786ba82c32a9fd6de9
Jon Park <jon@jonathanpark.ca>
2022-04-13 22:09:56 -0400

    testing pre-commit

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-const test = (a: string, b: number): string => a;
+const test = (a: string, b: number): string => console.log(a);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const test1 = test(2, 3);
+  const test1 = test(2, "test");
   return <Component {...pageProps} />;
 }
 
